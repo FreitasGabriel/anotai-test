4c3c2c29851887bae732df8bd2ebbccdbf922d70
Gabriel Freitas <gabriielfs96@gmail.com>
2024-07-10 18:17:35 -0300

    fix: collection name

--- src/model/repository/product/create_product.go
+++ src/model/repository/product/create_product.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"os"
+
 	productDomain "github.com/FreitasGabriel/anotai-test/src/model/domain/product"
 	"github.com/FreitasGabriel/anotai-test/src/model/repository/entity/converter"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -8,7 +10,8 @@ import (
 
 func (pr *productRepositoryInterface) CreateProduct(product productDomain.ProductDomainInterface) (*mongo.InsertOneResult, error) {
 
-	colletion := pr.databaseConn.Collection(COLLECTION_NAME)
+	collection_name := os.Getenv(COLLECTION_NAME)
+	colletion := pr.databaseConn.Collection(collection_name)
 
 	value := converter.ProductDomainToEntity(product)
 
